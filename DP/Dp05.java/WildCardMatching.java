981b960f8f712542dc69536e2030f1c4861d0767
rupant-rai <156572366+rupant-rai@users.noreply.github.com>
2025-04-24 14:01:02 +0530

    Wild Card Matching

---
+++ DP/Dp05.java/WildCardMatching.java
@@ -0,0 +1,47 @@
+public class WildCardMatching {
+    public static boolean Wild(String s, String p){
+        int n=s.length();
+        int m=p.length();
+
+        boolean dp[][]= new boolean[n+1][m+1];
+
+        //initilaization
+        dp[0][0]= true;
+
+        for( int i=1; i<n+1; i++){
+            dp[i][0]= false;
+        }
+        for( int j=1; j<m+1; j++){
+            if(p.charAt(j-1)=='*'){
+                dp[0][j]= dp[0][j-1];
+            }
+            else{
+                dp[0][j]= false;
+            }
+        }
+
+
+        // fill the form int the bottom up fashion
+
+        for ( int i=1; i<n+1; i++){
+            for ( int j=1; j<m+1; j++){
+                if(s.charAt(i-1)==p.charAt(j-1)|| p.charAt(j-1)=='?'){
+                    dp[i][j]= dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*'){
+                    dp[i][j]= dp[i][j-1];
+                }
+                else{
+                    dp[i][j]= false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main( String args[]){
+        String s= "abaaabab";
+        String p= "***?ba***ba?";
+        System.out.println(Wild(s,p));
+    }
+}
